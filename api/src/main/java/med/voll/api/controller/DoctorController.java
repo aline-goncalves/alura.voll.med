f901fd5b7801b9676ef955c4da1a348e18fb929b
aline-goncalves <toneti.aline@gmail.com>
2023-04-14 11:30:50 -0300

    adding registrations list with pagination and sort

--- api/src/main/java/med/voll/api/controller/DoctorController.java
+++ api/src/main/java/med/voll/api/controller/DoctorController.java
@@ -4,7 +4,11 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
@@ -18,5 +22,16 @@ public class DoctorController {
         repository.save(new Doctor(data));
         System.out.println(data);
         return "Doctor created successfully!";
+    }
+
+    @GetMapping
+    @RequestMapping("/all")
+    public List<DoctorListRecord> listAllDoctors(){
+        return repository.findAll().stream().map(DoctorListRecord::new).toList();
+    }
+
+    @GetMapping
+    public Page<DoctorListRecord> listAllDoctors(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable){
+        return repository.findAll(pageable).map(DoctorListRecord::new);
     }
 }
--- api/src/main/java/med/voll/api/controller/PatientController.java
+++ api/src/main/java/med/voll/api/controller/PatientController.java
@@ -4,7 +4,12 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.patient.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -18,5 +23,16 @@ public class PatientController {
         repository.save(new Patient(data));
         System.out.println(data);
         return "Patient created successfully!";
+    }
+
+    @GetMapping
+    @RequestMapping("/all")
+    public List<PatientListRecord> listAllPatients(){
+        return repository.findAll().stream().map(PatientListRecord::new).toList();
+    }
+
+    @GetMapping
+    public Page<PatientListRecord> listAllPatients(Pageable pageable){
+        return repository.findAll(pageable).map(PatientListRecord::new);
     }
 }
